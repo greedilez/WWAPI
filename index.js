7d7e743ed68747267368963f073125354ab8a113
Timur <96943622+greedilez@users.noreply.github.com>
2025-09-30 22:33:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/offer", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     image_url: "https://raw.githubusercontent.com/greedilez/WWAPI/main/360_F_198733249_hjyzxGkoeqCmBw5ZBE1j8T0wj8oO2r2Z.jpg",
     offer_url: "https://example.com/offer"
